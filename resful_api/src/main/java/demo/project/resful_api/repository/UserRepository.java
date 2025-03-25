eb1d10bdf1055abfd0712a1e472a802818346bb9
Võ Chí Hải <vochihai7@gmail.com>
2025-03-25 20:40:56 +0700

    Delete resful_api directory

--- resful_api/src/main/java/demo/project/resful_api/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package demo.project.resful_api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import demo.project.resful_api.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
